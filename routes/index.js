d7783d9d0fe42bfe8472c47d42bc313cac434ba7
AshishManer <ashishmaner94@gmail.com>
2022-10-10 15:55:27 +0530

    my yhird commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const homeController = require('../controllers/home_controller')
+
+//route for homepage
+router.get('/', homeController.home)
+
+//route for all other routes related to file
+router.use('/file', require('./file'))
+
+module.exports = router
